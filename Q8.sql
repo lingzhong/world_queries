21cda134177417a22f45dce63ef186e86a35d1be
Aleck Wu <Aleck@Alecks-MacBook-Pro.local>
2014-11-09 14:14:01 -0500

    modified Q8.sql

--- Q8.sql
+++ Q8.sql
@@ -9,14 +9,13 @@ FROM language l2
 GROUP BY l2.cid);
 
 INSERT INTO Query8
-SELECT c1.cname as c1name, c2.cname as c2name, l.lname as lname
+SELECT DISTINCT c1.cname as c1name, c2.cname as c2name, l.lname as lname
 FROM country c1, most_popular_language mpl1, 
 neighbour n, language l,
 country c2, most_popular_language mpl2
 WHERE mpl1.cid = n.country AND c1.cid = mpl1.cid 
 AND mpl2.cid = n.neighbor AND c2.cid = mpl2.cid
 AND mpl1.lid = mpl2.lid and mpl1.lid = l.lid
-GROUP BY c1name, c2name, lname
 ORDER BY lname ASC, c1name DESC;
 
 DROP VIEW most_popular_language;
